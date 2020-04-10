028f11b6d1d7acba4792f986d7293e0954a5aba7
Pablo <royadeveloper01@gmail.com>
2020-04-10 00:22:23 +0000

    intial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function users()
+    {
+        $users = User::get();
+        return view('users', compact('users'));
+    }
+
+    public function user($id, $name)
+    {
+        $user = User::find($id, $name);
+        return view('usersView', compact('user'));
+    }
+
+    public function follwUserRequest(Request $request){
+
+
+        $user = User::find($request->user_id);
+        $response = auth()->user()->toggleFollow($user);
+
+
+        return response()->json(['success'=>$response]);
+    }
+}
